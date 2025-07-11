26912f2ff48d9064059a29e4c57a13cdf01c527b
arjunop21 <arjunopsreesiva21@gmail.com>
2025-07-11 11:46:09 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,6 @@ import { connect } from 'mongoose';
 const connectDB = async () => {
   try {
     await connect(process.env.MONGO_URI, {
-      retryWrites: false, // Disable retryWrites for better performance in high-load scenarios
       maxPoolSize: 100,
       writeConcern: { w: 'majority' }, // Ensure data consistency
     });
